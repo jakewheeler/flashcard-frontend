0391d580c627a7854d0a762f7d3c17f183cab240
Jake Wheeler <jakewheeler17@gmail.com>
2020-08-24 19:58:24 -0400

    work on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text, VStack, HStack, Heading } from '@chakra-ui/core';
+import { Text, VStack, Heading } from '@chakra-ui/core';
+import Card, { ResponsiveCardLayout } from '../components/Card';
 
 export default function Home() {
   return (
@@ -7,7 +8,12 @@ export default function Home() {
       <Heading size='2xl' color='teal.900'>
         Welcome to Flashy Cards
       </Heading>
-      <HStack></HStack>
+      <Text>What will you learn today?</Text>
+      <ResponsiveCardLayout>
+        <Card url=''>Art</Card>
+        <Card url=''>Science</Card>
+        <Card url=''>History</Card>
+      </ResponsiveCardLayout>
     </VStack>
   );
 }
