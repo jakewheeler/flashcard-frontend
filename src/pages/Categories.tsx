1b327a18712fed074595312f1550db5a60b60a9b
Jake Wheeler <jakewheeler17@gmail.com>
2020-08-25 21:10:59 -0400

    working on layout for library

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { useQuery } from 'react-query';
-import { getCategories } from '../api/cardService';
+import { getCategories, Category } from '../api/cardService';
 import useStore from '../utils/user';
-import { Box } from '@chakra-ui/core';
+import {
+  Box,
+  Heading,
+  Flex,
+  RadioGroup,
+  Radio,
+  Text,
+  VStack,
+  Divider,
+  HStack,
+  Button,
+  Collapse,
+} from '@chakra-ui/core';
 import Card, { ResponsiveCardLayout } from '../components/Card';
 
 function useCategories() {
@@ -22,12 +34,75 @@ export default function Categories() {
   }
 
   return (
-    <ResponsiveCardLayout>
-      {data?.map((category) => (
-        <Box key={category.id}>
-          <Card url={`/categories/${category.id}/decks`}>{category.name}</Card>
-        </Box>
-      ))}
-    </ResponsiveCardLayout>
+    <Flex>
+      <MenuSection />
+      <ResponsiveCardLayout>
+        {data?.map((category) => (
+          <Box key={category.id}>
+            <Card url={`/categories/${category.id}/decks`}>
+              {category.name}
+            </Card>
+          </Box>
+        ))}
+      </ResponsiveCardLayout>
+    </Flex>
+  );
+}
+
+function MenuSection() {
+  const { isLoading, error, isError, data } = useCategories();
+
+  if (isError) {
+    return <span>Error: {(error as Error).message}</span>;
+  }
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  return (
+    <Box minH='100vh' minW='300px' maxW='300px' bgColor='teal.400'>
+      <Heading align='center' mt='35px' color='teal.100' mb='5px'>
+        Library
+      </Heading>
+      <Divider />
+      <VStack spacing='5px' mx='20px' mt='20px' align='left'>
+        <Text align='left' color='teal.100'>
+          View by{' '}
+        </Text>
+        <RadioGroup defaultValue='1'>
+          <HStack spacing={5}>
+            <Radio colorScheme='teal' value='1'>
+              Category
+            </Radio>
+            <Radio colorScheme='teal' value='2'>
+              Deck
+            </Radio>
+          </HStack>
+        </RadioGroup>
+      </VStack>
+      <VStack align='left' paddingTop={5} mr={5} ml={5}>
+        {data?.map((category) => (
+          <CollapsibleCategory key={category.id} category={category} />
+        ))}
+      </VStack>
+    </Box>
+  );
+}
+
+function CollapsibleCategory({ category }: { category: Category }) {
+  const [show, setShow] = React.useState<boolean>(false);
+
+  const handleToggle = () => setShow(!show);
+
+  return (
+    <>
+      <Button colorScheme='teal' onClick={handleToggle}>
+        <Text isTruncated> {category.name}</Text>
+      </Button>
+      <Collapse mt={4} isOpen={show}>
+        {category.id}
+      </Collapse>
+    </>
   );
 }
