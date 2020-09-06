1441aa3704389749bb5c28a9ac672cff3a57dad5
Jake Wheeler <jakewheeler17@gmail.com>
2020-09-05 22:54:33 -0400

    add 'me' route to api

--- src/api/login-service.ts
+++ src/api/login-service.ts
@@ -9,3 +9,17 @@ export async function fetchToken(user: UserData): Promise<string> {
   const data = (await axios.post<Token>('/auth/signin', user)).data.accessToken;
   return data;
 }
+
+export async function tryFetchLoggedInUser(token: string): Promise<string> {
+  const response = await axios.get<string>('/auth/me', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (response.status === 200) {
+    return response.data;
+  }
+
+  throw new Error('User token is not valid.');
+}
